eeb5cef73018018b71db4b404345ab2e8fc5df41
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2023-11-07 21:29:41 +0100

    Use history API to implement no refresh navigations (#3)
    * set up base server for spa
    
    * setup basic router functionality
    
    * use history API to implement no refresh navigation

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,8 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
 const router = async () => {
     const routes = [
         {path: "/", view: () => console.log("Viewing Home")},
@@ -26,6 +31,14 @@ const router = async () => {
     console.log(match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if(e.target.matches("[data-link]")){
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
     router();
 })
