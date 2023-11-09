ef7c575b775b166b00a3e5bb6ef88217e9a2da33
ernest-lamptey <99683999+ernest-lamptey@users.noreply.github.com>
2023-11-09 20:48:26 +0100

    Setup other pages (#5)
    * set up base server for spa
    
    * setup basic router functionality
    
    * use history API to implement no refresh navigation
    
    * setup views
    
    * connect other pages

--- frontend/static/js/views/Contact.js
+++ frontend/static/js/views/Contact.js
@@ -3,14 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
         super();
-        this.setTitle("Home");
+        this.setTitle("Contact");
     }
 
     async getHtml(){
         return `
-            <h1>Homepage</h1>
-            <p>This is the homepage</p>
-            <p><a href="/work" data-link>Work</a></p>
+            <h1>Contact</h1>
+            <p>This is the contact page</p>
+            <p><a href="/" data-link>Home</a></p>
         `
     }
 }
--- frontend/static/js/views/NotFound.js
+++ frontend/static/js/views/NotFound.js
@@ -3,14 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
         super();
-        this.setTitle("Home");
+        this.setTitle("404");
     }
 
     async getHtml(){
         return `
-            <h1>Homepage</h1>
-            <p>This is the homepage</p>
-            <p><a href="/work" data-link>Work</a></p>
+            <h1>Missing Page</h1>
+            <p>Sorry, we could not find that page</p>
+            <p><a href="/" data-link>Home</a></p>
         `
     }
 }
